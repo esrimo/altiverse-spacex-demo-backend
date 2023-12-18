c1ce0f73f669d26ee5dd9e7b539899586ebbf0fc
esrimo <esteban.rincon.mora@gmail.com>
2023-12-17 20:26:27 -0500

    altiverse-tech-assessment-esteban-rincon: feat(Missions): Setup missions for gql

---
+++ routes/graphql/resolvers/mission.ts
@@ -0,0 +1,15 @@
+import { QueryMissionArgs } from '../../../common/types/backend';
+import { GraphqlContext } from '../../../config';
+import { missionController } from '../../../controllers';
+import { MissionAttributes } from '../../../models/Mission';
+
+const missions = async (rootValue, { shipId }: QueryMissionArgs, context: GraphqlContext): Promise<MissionAttributes[]> => {
+  return missionController.get(shipId);
+};
+
+const query = { missions };
+
+const mutation = {};
+
+const Mission = { query, mutation };
+export { Mission };
